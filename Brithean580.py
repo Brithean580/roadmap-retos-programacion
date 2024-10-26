0a56197115d9735b977b262c61fba37d41439f7f
Brithean580 <britheanm@gmail.com>
2024-10-26 13:24:04 -0400

    Create Brithean580.py

---
+++ Brithean580.py
@@ -0,0 +1,22 @@
+# https://.python.org
+
+# Esto es un comentario en una linea
+
+"""
+Estos es una comentario
+en varias lineas
+"""
+
+my_variable = "el teclado no funciona"
+
+MY CONSTANT = "el teclado sigue sin funcionar" # por convención
+
+my_ int = 1
+my_ float = 1.5
+
+my_bool = True
+my_bool = false
+my_string = "el teclado sigue sin funcionar"
+my_other_string = el teclado no tiene reparación"
+
+print("Hola, Python!") 
